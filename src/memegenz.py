3cce0dc4b9a0408b6baa9486eccc82354e2bca43
Jason Byttow <jasonbyttow@gmail.com>
2012-12-25 14:16:36 -0800

    wip

--- src/memegenz.py
+++ src/memegenz.py
@@ -1,10 +1,26 @@
-import webapp2
+import webapp2, os, logging
+from google.appengine.ext.webapp import template
 
-class MainPage(webapp2.RequestHandler):
+class MainHandler(webapp2.RequestHandler):
   def get(self):
-    self.response.headers['Content-Type'] = 'text/plain'
+    self.response.headers['Content-Type'] = 'text/html'
     self.response.write('Hello, World!')
 
-app = webapp2.WSGIApplication([
-  ('/', MainPage)],
+class EditorHandler(webapp2.RequestHandler):
+  def get(self):
+    logging.info("editor")
+    
+    self.response.headers['Content-Type'] = 'text/html'
+    self.response.write('Hello, World!')
+    
+    #template_values = {}
+    #path = os.path.join(os.path.dirname(__file__), 'templates/editor.html')
+    #logging.info("template path: %s", path)
+    #self.response.out.write(template.render(path, template_values))
+
+app = webapp2.WSGIApplication(
+  [ 
+    ('/', MainHandler),
+    ('/editor', EditorHandler),
+  ],
   debug=True)
