dcad14052cf001835397911f988159d4ce2f5208
Jason Byttow <jasonbyttow@gmail.com>
2012-12-26 15:05:44 -0800

    editor wip

--- src/handlers/routes.py
+++ src/handlers/routes.py
@@ -2,11 +2,13 @@
 from index import *
 from meme import *
 from template import *
+from editor import *
 
 
 ROUTES = [
   ('/', IndexHandler),
   ('/meme', MemeHandler),
+  ('/editor', EditorHandler),
   ('/template', TemplateHandler),
   ('/templates', GetTemplatesHandler),
 ]
