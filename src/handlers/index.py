763dbfdd90555355c36d5cdc221db6370e611c96
David Byttow <d@d.local>
2012-12-25 21:06:51 -0600

    Variable example

--- src/handlers/index.py
+++ src/handlers/index.py
@@ -6,4 +6,4 @@ from mako.template import Template
 class IndexHandler(webapp2.RequestHandler):
   def get(self):
     template = Template(filename='templates/index.html')
-    self.response.write(template.render())
+    self.response.write(template.render(person='Jason'))
