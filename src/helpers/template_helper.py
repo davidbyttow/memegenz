ee78873454553aadae1fbcba77a5a96a50075e18
David Byttow <d@d.local>
2012-12-26 22:38:59 -0600

    Minor changes

--- src/helpers/template_helper.py
+++ src/helpers/template_helper.py
@@ -6,7 +6,9 @@ import sys
 from mako.template import Template
 from mako.lookup import TemplateLookup
 
+
 lookup = TemplateLookup(directories=['templates'])
+
 
 def render(template_name, **kwargs):
   template = lookup.get_template(template_name)
